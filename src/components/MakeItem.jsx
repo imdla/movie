f1379fefda2e6a3bddeae7bef51ffac645e01c9f
imdla <imdla918@gmail.com>
2024-11-18 17:41:46 +0900

    [refactor] components / MakeItems.jsx & pages / MovieItem.jsx & MovieType.jsx

--- src/components/MakeItem.jsx
+++ src/components/MakeItem.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import movieApi from "../api/movieApi";
 import MovieItem from "./MovieItem";
 
-export default function MakeItem({ movieList, count }) {
+export default function MakeItem({ listType, count }) {
+  const [movieList, setMovieList] = useState();
+
+  useEffect(() => {
+    async function getMovieList() {
+      try {
+        const data = await movieApi.getMovies(listType);
+        setMovieList(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+      }
+    }
+    getMovieList();
+  });
+
+  if (!movieList) {
+    return <div>로딩중</div>;
+  }
+
   let cnt = 0;
   const movieItems = movieList.map((movieItem) => {
     if (cnt < count) {
--- src/pages/MovieListsItem.jsx
+++ src/pages/MovieListsItem.jsx
@@ -1,32 +1,11 @@
 import React from "react";
-import MovieItem from "../components/MovieItem";
-import { useEffect, useState } from "react";
-import movieApi from "../api/movieApi";
 import { useNavigate } from "react-router-dom";
 import MakeItem from "../components/MakeItem";
 
 export default function MovieListsItem({ listType }) {
-  const [movieList, setMovieList] = useState();
   const navigate = useNavigate();
 
-  useEffect(() => {
-    async function getMovieList() {
-      try {
-        const data = await movieApi.getMovies(listType);
-        setMovieList(data);
-      } catch (err) {
-        console.error(err);
-      } finally {
-      }
-    }
-    getMovieList();
-  });
-
-  if (!movieList) {
-    return <div>로딩중</div>;
-  }
-
-  let movieItems = <MakeItem movieList={movieList} count={5}></MakeItem>;
+  let movieItems = <MakeItem listType={listType} count={5}></MakeItem>;
 
   return (
     <>
