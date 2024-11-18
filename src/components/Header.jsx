0617ebdc4f8e63cdec36788c6036e3df22f494dc
imdla <imdla918@gmail.com>
2024-11-18 11:31:18 +0900

    [feat] components / Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1>Movie Page</h1>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="">LOGIN</a>
+          </li>
+          <li>
+            <a href="">MY PAGE</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function RootLayout() {
   return (
     <>
-      <header>헤더</header>
+      <Header></Header>
       <Outlet></Outlet>
       <footer>푸터</footer>
     </>
