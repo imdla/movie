64d708def084e20afb6af7f4730c8c7ec08411bd
imdla <imdla918@gmail.com>
2024-11-18 17:29:24 +0900

    [refactor] pages / MovieType.jsx

--- src/pages/MovieType.jsx
+++ src/pages/MovieType.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import MakeItem from "../components/MakeItem";
+import movieApi from "../api/movieApi";
+import { useEffect, useState } from "react";
 
 export default function MovieType() {
   const { listType } = useParams();
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    async function getMovieList() {
+      try {
+        const data = await movieApi.getMovies(listType);
+        setMovieList(data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+      }
+    }
+    getMovieList();
+  });
+
+  if (!movieList) {
+    return <div>로딩중</div>;
+  }
+
+  let movieItems = <MakeItem movieList={movieList} count={10}></MakeItem>;
 
   return (
     <>
       <h2>Movie Type - {listType}</h2>
-      
+      {movieItems}
     </>
   );
 }
