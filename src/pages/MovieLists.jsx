c6ca806bfda9f565949b7259ba1378526731ce73
imdla <imdla918@gmail.com>
2024-11-18 14:38:51 +0900

    [refactor] pages / MovieLists.jsx -> modify api

--- src/pages/MovieLists.jsx
+++ src/pages/MovieLists.jsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import MovieListsItem from "../components/MovieListsItem";
+import { useEffect } from "react";
+import movieApi from "../api/movieApi";
 
 export default function MovieLists() {
+  const [movieList, setMovieList] = useState();
+  const type = "now_playing"
+
+  useEffect(() => {
+    async function getMovieList() {
+      try {
+        const data = movieApi.getMovies({ type });
+        const movieLists = data.results
+        setMovieList(movieLists)
+      } catch (err) {
+        console.log("api 에러!!!!!!!!!!!")
+      } finally {
+      }
+    }
+    getMovieList();
+  });
+
   return (
     <>
-      <MovieListsItem listType="popular"></MovieListsItem>
+      <MovieListsItem listType={type}></MovieListsItem>
     </>
   );
 }
