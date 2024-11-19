b24462502e43a56e0926494122138636e99493cd
imdla <imdla918@gmail.com>
2024-11-19 10:25:58 +0900

    [feat] pages / MyPage.jsx 로그인 검사 기능 추가

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 export default function MyPage() {
+  const navigate = useNavigate();
+  const { isLoggedIn } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      alert("로그인이 필요합니다.");
+      navigate("/login/request");
+    }
+  }, [isLoggedIn]);
+
   return (
     <>
       <h2>My Favorite Movie Page</h2>
