03c224f32c3dc67576ae886851d3ec58b85a23e4
imdla <imdla918@gmail.com>
2024-11-20 10:20:48 +0900

    [modify] components / MovieItem.jsx 코드 위치 수정

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import imgUrl from "../utills/imgUrl";
 import { Link } from "react-router-dom";
+import imgUrl from "../utills/imgUrl";
 
 export default function MovieItem({ movieItem }) {
   const { id, title, overview, poster_path } = movieItem;
+
   return (
     <>
       <Link to={`/movie/detail/${id}`}>
