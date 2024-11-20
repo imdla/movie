0122ac02c5a9321df0ba43f8eef55aef35b401ce
imdla <imdla918@gmail.com>
2024-11-20 11:05:34 +0900

    [feat] pages / MovieType.jsx 페이지 더보기 기능 추가

--- src/pages/MovieType.jsx
+++ src/pages/MovieType.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import MakeItem from "../components/MakeItem";
+import { typeListAmount } from "../utills/movieInfo";
+import { useState } from "react";
 
 export default function MovieType() {
   const { listType } = useParams();
-  let movieItems = <MakeItem listType={listType} count={10}></MakeItem>;
+  const [cnt, setCnt] = useState(typeListAmount);
+
+  let movieItems = <MakeItem listType={listType} count={cnt}></MakeItem>;
+
+  function handleClick() {
+    setCnt(cnt + 5);
+  }
 
   return (
     <div className="container movieType">
       <h2>Movie Type - {listType}</h2>
-      <ul className="ulTag flex-center flex-wrap">{movieItems}</ul>
+      <ul className="ulTag flex-center flex-wrap">
+        {movieItems}
+        <button onClick={handleClick}>더보기</button>
+      </ul>
     </div>
   );
 }
--- src/utills/movieInfo.jsx
+++ src/utills/movieInfo.jsx
@@ -1,3 +1,5 @@
 export const movieTypeList = ["now_playing", "popular", "top_rated"];
 
 export const typeAmount = 15;
+
+export const typeListAmount = 5;
