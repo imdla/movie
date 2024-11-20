082abd687c948cb5771d0b5ad662fe6c9868e675
imdla <imdla918@gmail.com>
2024-11-20 09:18:50 +0900

    [refactor] pages / MovieLists.jsx 링크 수정

--- src/pages/MovieLists.jsx
+++ src/pages/MovieLists.jsx
@@ -2,7 +2,7 @@ import MovieListsItem from "../components/MovieListsItem";
 import { movieTypeList } from "../utills/movieInfo";
 
 export default function MovieLists() {
-  const movieTypes = movieTypeList.map((type) => {
+  const movieTypes = movieTypeList().map((type) => {
     return (
       <section className="movieList" key={type}>
         <MovieListsItem listType={type}></MovieListsItem>
