2992ee3c3fba35a44aff29de26c40de94750e062
imdla <imdla918@gmail.com>
2024-11-21 17:06:15 +0900

    [feat] 로그인 하지 않았을 때 confirm 창을 띄워 로그인 창으로 넘어갈지 말지 기능 추가

--- src/components/SaveButton.jsx
+++ src/components/SaveButton.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { saved, remove } from "../store/slices/movieSaveSlice";
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SaveButton({ isSaved, setIsSaved, movieId }) {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state) => state.auth);
   const { saveMovieId } = useSelector((state) => state.movieSave);
@@ -19,7 +21,9 @@ export default function SaveButton({ isSaved, setIsSaved, movieId }) {
         alert("MY PAGE에 저장되었습니다.");
       }
     } else {
-      alert("로그인 후 이용 가능합니다.");
+      if (window.confirm("로그인 후 이용 가능합니다. 로그인 하시겠습니까?")) {
+        navigate("/login");
+      }
     }
   }
 
