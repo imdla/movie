47f609d9e10e0b07b9021b269aba4eb3323c36d0
imdla <imdla918@gmail.com>
2024-11-21 17:00:01 +0900

    [add] Header.jsx의 해시 태그 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,28 +6,49 @@ export default function Header() {
   const { isLoggedIn, authName } = useSelector((state) => state.auth);
 
   return (
-    <header className="flex-center justy-between container">
-      <div>
-        <Link to={"/"}>
-          <h1 className="text-center logo">Movie Page</h1>
-        </Link>
+    <header className="container">
+      <div className="flex-center justy-between ">
+        <div>
+          <Link to={"/"}>
+            <h1 className="text-center logo">Movie Page</h1>
+          </Link>
+        </div>
+
+        <nav>
+          <ul className="ulTag flex-center">
+            <li></li>
+            <li>
+              <Link to={"/login"}>
+                <h4>{isLoggedIn ? "로그아웃" : "로그인"}</h4>
+              </Link>
+            </li>
+            <li>
+              <Link to={"/mypage"}>
+                <h4 className="myPage">
+                  {isLoggedIn ? `${authName}님` : "MY PAGE"}
+                </h4>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
 
-      <nav>
-        <ul className="ulTag flex-center">
-          <li></li>
+      <div>
+        <ul className="flex-center ulTag justy-start hash">
           <li>
-            <Link to={"/login"}>
-              <h4>{isLoggedIn ? "로그아웃" : "로그인"}</h4>
-            </Link>
+            <Link to={"/movie/now_playing"}># 현재 상영</Link>
           </li>
           <li>
-            <Link to={"/mypage"}>
-              <h4 className="myPage">{isLoggedIn ? `${authName}님` : "MY PAGE"}</h4>
-            </Link>
+            <Link to={"/movie/popular"}># 인기있는</Link>
+          </li>
+          <li>
+            <Link to={"/movie/top_rated"}># 순위별</Link>
+          </li>
+          <li>
+            <Link to={"/movie/top_rated"}># 장르별</Link>
           </li>
         </ul>
-      </nav>
+      </div>
     </header>
   );
 }
