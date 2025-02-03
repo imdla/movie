00669e795016fcde4b5c114e33cc2d7c9d0ebc36
imdla <imdla918@gmail.com>
2025-02-03 22:37:17 +0900

    [modify] 코드 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import SearchBox from "./SearchBox";
 
