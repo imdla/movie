a71cd4ba355b4e6eb4b026e3ef986573cd38dd38
imdla <imdla918@gmail.com>
2025-02-03 22:40:05 +0900

    [add] Signup 기초 기능

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Signup() {
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({ name: "", password: "" });
+  const { isLoggedIn, userName, userPassword } = useSelector(
+    (state) => state.auth.user
+  );
+
+  function handleChange(e) {
+    return setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const { name, password } = formData;
+
+    if (isLoggedIn) {
+      dispatch(logout());
+    } else {
+      if (userName == name && userPassword == password) {
+        const loginData = {
+          isLoggedIn: true,
+          userName: name,
+          userPassword: password,
+          movieList: [],
+        };
+        dispatch(login(loginData));
+        setFormData({ name: "", password: "" });
+        navigate("/");
+      } else {
+        alert("아이디나 비밀번호가 올바르지 않습니다.");
+      }
+    }
+  }
+
   return (
     <div className="container">
-      <h2>Login Page</h2>
+      <h2>Signup Page</h2>
 
       <form className="flex-center flex-col" action="" onSubmit={handleSubmit}>
         <div className="marginBttom flex-center">
@@ -35,6 +69,17 @@ export default function Signup() {
             id="password"
             name="password"
             value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className="marginBttom flex-center">
+          <label htmlFor="password">PASSWORD CHECK</label>
+          <input
+            type="password"
+            id="passwordCheck"
+            name="passwordCheck"
+            value={formData.passwordCheck}
             onChange={handleChange}
           />
         </div>
