bf6fabca3d2b2b9237df985f02daff827e44e034
imdla <imdla918@gmail.com>
2025-02-15 23:26:02 +0900

    [fix] 폰트 환경 변수 지정 변경

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+window.fontKey = import.meta.env.VITE_FONT_API_KEY;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
