742c810404e06a4c329b9c2dd34e8d1ce8f96ce0
imdla <imdla918@gmail.com>
2025-02-16 23:09:40 +0900

    [style] footer 내용 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <h3>영화 홈페이지</h3>
-      <p>TMDB API를 활용한 영화 홈페이지 입니다</p>
-      <p>Copyright 2025. TMDB Movie Page. all rights reserved.</p>
+      <h3>Movie World</h3>
+      <p>TMDB API를 활용한 영화 홈페이지입니다</p>
+      <p>Copyright 2025. Movie World. all rights reserved.</p>
     </footer>
   );
 }
