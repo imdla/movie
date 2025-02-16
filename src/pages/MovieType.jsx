868d69e08f0c212f1cbaa64970c28d8f5525a924
imdla <imdla918@gmail.com>
2025-02-16 23:06:01 +0900

    [style] title style 변경

--- src/pages/MovieType.jsx
+++ src/pages/MovieType.jsx
@@ -1,13 +1,19 @@
 import { useState } from "react";
 import { useParams } from "react-router-dom";
-import { typeListAmount } from "../utills/movieUtils";
+import {
+  movieTypeList,
+  movieTypeListKr,
+  typeListAmount,
+} from "../utills/movieUtils";
 import MovieItem from "../components/MovieListItem";
 
 export default function MovieType() {
   const { listType } = useParams();
   const [cnt, setCnt] = useState(typeListAmount);
 
-  let movieItems = <MovieItem listType={listType} count={cnt}></MovieItem>;
+  const index = movieTypeList.indexOf(listType);
+  const movieTitle = movieTypeListKr[index];
+  const movieItems = <MovieItem listType={listType} count={cnt}></MovieItem>;
 
   function handleClick() {
     setCnt(cnt + typeListAmount);
@@ -15,7 +21,7 @@ export default function MovieType() {
 
   return (
     <div className="container movieType">
-      <h2>{listType.replace("_", " ").toUpperCase()}</h2>
+      <h2>{movieTitle}</h2>
       <ul className="ulTag flex-center flex-wrap">
         {movieItems}
         <button onClick={handleClick}>더보기</button>
