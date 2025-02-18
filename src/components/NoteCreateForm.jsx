3d3e8d1b23a161a6624bc7f6d103146a836e31a1
imdla <imdla918@gmail.com>
2025-02-18 21:14:19 +0900

    [feat] NoteEditor 생성

--- src/components/NoteCreateForm.jsx
+++ src/components/NoteCreateForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import MemoEditor from "./MemoEditor";
+import NoteEditor from "./NoteEditor";
 
 function NoteCreateForm({
   title,
