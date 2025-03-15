6f985d5ab7f5721e2515aa71936bde09192f6190
imdla <imdla918@gmail.com>
2025-03-16 00:48:27 +0900

    [feat] CustomAccessDeniedHandler 생성

--- back/src/main/java/com/example/back/global/config/SecurityConfig.java
+++ back/src/main/java/com/example/back/global/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.back.global.config;
 
 import com.example.back.global.security.JwtAuthenticationFilter;
+import com.example.back.global.security.handler.CustomAccessDeniedHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
