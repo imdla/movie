9dc28d8f6abe70726c4b2d4f8da10b4d489d5b06
imdla <imdla918@gmail.com>
2025-03-16 00:38:11 +0900

    [feat] JwtAuthenticationFilter 생성

--- back/src/main/java/com/example/back/global/config/SecurityConfig.java
+++ back/src/main/java/com/example/back/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.back.global.config;
 
+import com.example.back.global.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
